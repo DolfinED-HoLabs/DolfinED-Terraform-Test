552dd8221ac4f8f11e21a4d7fe7b79ef12d05945
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-06-22 00:31:34 +0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,10 +3,10 @@ provider "aws" {
 region = "us-east-1"
 }
 
-# resource "aws_instance" "webapp" {
-#  ami           = "ami-00beae93a2d981137"
-#  instance_type = "t2.micro"
-#}
+resource "aws_instance" "webapp" {
+ ami           = "ami-08a0d1e16fc3f61ea"
+ instance_type = "t2.micro"
+}
 
 resource "aws_s3_bucket" "dolfined_bucket" {
   bucket = "my-tf-test12345-bucket-12345"
