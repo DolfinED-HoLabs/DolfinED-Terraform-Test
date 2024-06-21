c981a70c1654ff90d97aadabd1c95ed941c2ca20
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-06-22 00:27:36 +0400

    Update ec2.tf
    added s3 bucket

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,14 @@
 
-provider "aws" {}
-
-resource "aws_instance" "webapp" {
-  ami           = "ami-00beae93a2d981137"
-  instance_type = "t2.micro"
+provider "aws" {
+region = "us-east-1"
 }
+
+# resource "aws_instance" "webapp" {
+#  ami           = "ami-00beae93a2d981137"
+#  instance_type = "t2.micro"
+#}
+
+resource "aws_s3_bucket" "dolfined_bucket" {
+  bucket = "my-tf-test12345-bucket-12345"
+}
+
