54c2cd4aef9558adec8422cf42e767961fc74e87
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-06-22 00:09:35 +0400

    Update ec2.tf
    removed access keys

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,5 @@
 
-provider "aws" {
-  region  = "us-east-1"
-  access_key = "AKIAYS2NUV67CCQTPA5N"
-  secret_key = "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"
-}
+provider "aws" {}
 
 
 resource "aws_instance" "webapp" {
