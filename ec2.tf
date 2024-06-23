19d0e84a2f25f243ae68b4aefedb27183f0c6996
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-06-23 20:17:28 +0400

    Restored the instance type

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 
 resource "aws_instance" "webapp" {
  ami           = "ami-08a0d1e16fc3f61ea"
- instance_type = "t2.large"
+ instance_type = "t2.micro"
   tags = {
     Name = "dolfined_demo123"
     Env = "prod"
