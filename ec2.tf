18a61d40591e3c961609072a71d51c5468505da1
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-06-23 19:54:56 +0400

    tag name change

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "webapp" {
  ami           = "ami-08a0d1e16fc3f61ea"
  instance_type = "t2.micro"
   tags = {
-    Name = "dolfined_demo"
+    Name = "dolfined_demo1"
   }
 }
 
