6e2751320df92271f42f09b92c20ac7fb3289bc1
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-06-29 18:01:41 +0400

    large

--- ec2.tf
+++ ec2.tf
@@ -5,14 +5,11 @@ region = "us-east-1"
 
 resource "aws_instance" "webapp" {
  ami           = "ami-08a0d1e16fc3f61ea"
- instance_type = "t2.micro"
+ instance_type = "t2.large"
   tags = {
     Name = "dolfined_demo123"
     Env = "prod"
   }
 }
 
-resource "aws_s3_bucket" "dolfined_bucket" {
-  bucket = "my-tf-test12345-bucket-12345"
-}
 
