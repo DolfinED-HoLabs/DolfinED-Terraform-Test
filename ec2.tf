ef6d1462016103bfba0bc9be8d3443e76eb2e6b7
DolfinED-HoLabs <e.abousherif@gmail.com>
2024-06-29 20:57:52 +0400

    changed EC2 to Large

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 
 resource "aws_instance" "webapp" {
  ami           = "ami-08a0d1e16fc3f61ea"
- instance_type = "t2.micro"
+ instance_type = "t2.large"
 
   tags = {
     Name = "dolfined_demo12"
